7b877f6d276496e51fe0c28b57edfd521a8ddb8d
idanmaman2 <maman@yatmal.co.il>
2022-08-09 21:33:45 +0300

    INIT'

---
+++ core.py
@@ -0,0 +1,65 @@
+#!python3
+# Usage -
+# 1. open cmd
+# 2. cd to the folder where these files are present
+# 3. type - python ytdown.py
+# the script will start working
+
+
+import os
+import subprocess
+from pytube import YouTube
+import random
+import requests
+import re
+import json
+import string
+
+
+#imp functions
+
+
+def foldertitle(url):
+
+    try:
+        res = requests.get(url)
+    except:
+        print('no internet')
+        return False
+
+    plain_text = res.text
+    title = (re.finditer("<title>(.*)</title>",plain_text))
+    return next(title).group(1).strip().lower().replace("|","").replace(" ","_").replace("\\","").replace("/","")
+   
+
+def link_snatcher(url):
+    query = requests.get(url).text
+    file = open("hello.html",'w',encoding='utf-8')
+    file.write(query)
+    file.close()
+    jsonObject =json.loads(next(re.finditer("var ytInitialData = (\{.*\}\}\});\<",query)).group(1))
+    contents = jsonObject["contents"]["twoColumnWatchNextResults"]["playlist"]["playlist"]
+    title = contents["title"]
+    print("title: " , title)
+    contents = contents["contents"]
+    DIR = os.getcwd()  + "\\" + foldertitle(url)
+    for playlistItem in map(lambda x : x["playlistPanelVideoRenderer"] , contents[:-1]): 
+        title = playlistItem["title"]["simpleText"]
+        linkId = playlistItem["videoId"]
+        print(f"\rtitle : {title} , id : {linkId} ",end="")
+        yt = YouTube(f"http://youtube.com/watch?v={linkId}")
+        stream = yt.streams.filter(type="audio",mime_type="audio/webm").first()
+        stream.download(DIR)
+
+                
+
+    
+
+file_name = input("enter json with links path")
+jsObject = json.load(open(file_name,'r'))
+for link in jsObject["links"]: 
+    try : 
+        link_snatcher(link)
+    except Exception as err:
+        print(err)
+        print(link + " failed")
